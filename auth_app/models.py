c33c60565adf4f886478abd1f39e573102f96a42
Kasa28 <agliullinaksenija@gmail.com>
2026-08-05 00:35:11 +0200

    Add custom Coderr user model and migration

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class CoderrUser(AbstractUser):
+    USER_TYPES = [
+        ("customer", "Customer"),
+        ("business", "Business"),
+    ]
+
+    type = models.CharField(max_length=50, choices=USER_TYPES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
 
 # Create your models here.
