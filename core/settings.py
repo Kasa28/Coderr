43115f244b95f36cd23dcd81f4efec8b52ee33aa
Kasa28 <agliullinaksenija@gmail.com>
2026-08-05 00:30:36 +0200

    create cors for host

--- core/settings.py
+++ core/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'auth_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "auth_app.CoderrUser"
+
+# Local frontend development servers (for example VS Code Live Server).
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
