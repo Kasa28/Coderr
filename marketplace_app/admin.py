c64d793c748ed6966c7215629968677cc0854d5b
Kasa28 <agliullinaksenija@gmail.com>
2026-08-05 13:36:44 +0200

    fix admin MarketplaceOffer

--- marketplace_app/admin.py
+++ marketplace_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Offer, OfferPackage
+from .models import MarketplaceOffer, OfferPackage
 
 
-admin.site.register(Offer)
+admin.site.register(MarketplaceOffer)
 admin.site.register(OfferPackage)
