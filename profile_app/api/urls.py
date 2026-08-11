acf4668470df5cb22304fb4c821761acd43ad728
Kasa28 <agliullinaksenija@gmail.com>
2026-08-11 23:26:38 +0200

    expand ProfileDetailView, BusinessProfileListView, CustomerProfileListView

---
+++ profile_app/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from profile_app.api.views import (ProfileDetailView, BusinessProfileListView, CustomerProfileListView)
+
+
+urlpatterns = [
+    path(
+        "profile/<int:user_id>/",
+        ProfileDetailView.as_view(),
+        name="profile-detail",
+    ),
+    path(
+        "profiles/business/",
+        BusinessProfileListView.as_view(),
+        name="business-profile-list",
+    ),
+    path(
+        "profiles/customer/",
+        CustomerProfileListView.as_view(),
+        name="customer-profile-list",
+    ),
+]
