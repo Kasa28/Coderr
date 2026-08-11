3dcfe7fb4b4d512dbe4909681050e0adb6dbcb83
Kasa28 <agliullinaksenija@gmail.com>
2026-08-11 14:11:42 +0200

    created class Profile

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,45 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+
+    file = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(
+        max_length=300,
+        blank=True,
+        default="",
+    )
+
+    tel = models.CharField(
+        max_length=30,
+        blank=True,
+        default="",
+    )
+
+    description = models.TextField(
+        blank=True,
+        default="",
+    )
+
+    working_hours = models.CharField(
+        max_length=140,
+        blank=True,
+        default="",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
