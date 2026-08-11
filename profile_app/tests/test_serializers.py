1fcac2f4f24d20c21594bb73b7da9ac2db5d6188
Kasa28 <agliullinaksenija@gmail.com>
2026-08-11 15:26:57 +0200

    check is db refresh after change

--- profile_app/tests/test_serializers.py
+++ profile_app/tests/test_serializers.py
@@ -32,3 +32,55 @@ class ProfileSerializerTest(TestCase):
         self.assertEqual(serializer.data["type"], "business")
         self.assertEqual(serializer.data["email"], "max@business.de")
         self.assertEqual(serializer.data["location"], "Berlin")
+
+
+
+    def test_serializer_updates_data(self):
+        user = User.objects.create_user(
+            username="max_business",
+            email="max@business.de",
+            password="testpassword123",
+            first_name="Max",
+            last_name="Mustermann",
+            type="business",
+        )
+
+        profile = Profile.objects.create(
+            user=user,
+            location="Berlin",
+            tel="123456789",
+            description="Business description",
+            working_hours="9-17",
+        )
+
+        data = {
+            "first_name": "Peter",
+            "last_name": "Schmidt",
+            "email": "peter@example.com",
+            "location": "Hamburg",
+            "tel": "987654321",
+            "description": "Neue Beschreibung",
+            "working_hours": "10-18",
+        }
+
+        serializer = ProfileSerializer(
+            profile,
+            data=data,
+            partial=True,
+        )
+
+        self.assertTrue(
+            serializer.is_valid(),
+            serializer.errors
+        )
+
+        serializer.save()
+        user.refresh_from_db()
+        profile.refresh_from_db()
+        self.assertEqual(user.first_name, "Peter")
+        self.assertEqual(user.last_name, "Schmidt")
+        self.assertEqual(user.email, "peter@example.com")
+        self.assertEqual(profile.location, "Hamburg")
+        self.assertEqual(profile.tel, "987654321")
+        self.assertEqual(profile.description, "Neue Beschreibung")
+        self.assertEqual(profile.working_hours, "10-18")
