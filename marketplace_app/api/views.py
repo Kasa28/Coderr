95ded63d32a9fae433800fdfc15ffd131ad45fe9
Kasa28 <agliullinaksenija@gmail.com>
2026-08-12 13:05:23 +0200

    created offerlist and IsAuthenticatedBusineesUser

--- marketplace_app/api/views.py
+++ marketplace_app/api/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
-from .serializers import OfferCreateSerializer
+from django.db.models import Min
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from marketplace_app.api.filters import OfferFilter
+from marketplace_app.api.paginations import OffersResultPagination
+from marketplace_app.api.permissions import IsAuthenticatedBusinessUser
+from marketplace_app.api.serializers import OfferSerializer
+from marketplace_app.models import MarketplaceOffer
 
-# Create your views here.
+
+class OffersListView(generics.ListCreateAPIView):
+    serializer_class = OfferSerializer
+    pagination_class = OffersResultPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = OfferFilter
+    search_fields = ["title", "description"]
+    ordering_fields = ["updated_at", "min_price"]
+    ordering = ["-updated_at"]
+
+    def get_queryset(self):
+        return (
+            MarketplaceOffer.objects
+            .select_related("user")
+            .prefetch_related("packages")
+            .annotate(
+                min_price=Min("packages__price"),
+                min_delivery_time=Min(
+                    "packages__delivery_time_in_days"
+                ),
+            )
+        )
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated(), IsAuthenticatedBusinessUser()]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
