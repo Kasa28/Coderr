5efa4fb044dcd3b4629a8fbd085a0edf9c7cd67d
Kasa28 <agliullinaksenija@gmail.com>
2026-08-19 20:29:57 +0200

    test: add offer title search test

--- marketplace_app/tests/test_offer_list.py
+++ marketplace_app/tests/test_offer_list.py
@@ -42,3 +42,19 @@ class GetMarketplaceOfferListTests(APITestCase):
         self.assertEqual(offer["min_price"], "100.00")
         self.assertEqual(offer["min_delivery_time"], 3)
         self.assertEqual(len(offer["details"]), 1)
+
+
+    def test_search_offer_by_title(self):
+        MarketplaceOffer.objects.create(
+            user=self.business_user,
+            title="Search Title",
+            description="Description",
+        )
+
+        response = self.client.get("/api/offers/?search=Search")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"],1)
+
+        found_offer = response.data["results"][0]
+
+        self.assertEqual(found_offer["title"],"Search Title")
