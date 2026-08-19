329ffe02be70810e15853a68e72f5eeb25743a0a
Kasa28 <agliullinaksenija@gmail.com>
2026-08-19 22:27:33 +0200

    addd models.py and make migrations

--- order_app/models.py
+++ order_app/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
 from django.db import models
+from marketplace_app.models import OfferPackage
 
-# Create your models here.
+class Order(models.Model):
+
+    OFFER_TYPE_CHOICES = [
+        ("basic", "Basic"),
+        ("standard", "Standard"),
+        ("premium", "Premium"),
+    ]
+
+    STATUS_CHOICES = [
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled")
+    ]
+
+    customer_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="customer_orders",
+    )
+
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="business_orders",
+    )
+
+    offer_detail = models.ForeignKey(
+        OfferPackage,
+        on_delete=models.PROTECT,
+        related_name="orders",
+    )
+
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=50, choices=OFFER_TYPE_CHOICES)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="in_progress")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - Customer {self.customer_user.username}"
