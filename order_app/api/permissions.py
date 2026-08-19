b548f12f7f340cdc1629fb8cf4e876cb6bb090e5
Kasa28 <agliullinaksenija@gmail.com>
2026-08-20 00:10:49 +0200

    test: add order creation test

--- order_app/api/permissions.py
+++ order_app/api/permissions.py
@@ -7,3 +7,25 @@ class CheckIsCustomerUser(BasePermission):
             request.user.is_authenticated
             and request.user.type == "customer"
         )
+
+
+class CheckIsBusinessUser(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.type == "business"
+        )
+
+
+class CheckBusinessOwnsOrder(BasePermission):
+
+    def has_object_permission(
+        self,
+        request,
+        view,
+        order,
+    ):
+        return (
+            order.business_user == request.user
+        )
--- order_app/api/urls.py
+++ order_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from order_app.api.views import OrdersView
+from order_app.api.views import OrdersView, OrderDetailView
 
 urlpatterns = [
     path("orders/", OrdersView.as_view(), name="order-list-create"),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
 ]
